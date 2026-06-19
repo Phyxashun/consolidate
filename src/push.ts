3d966b4d4fc462887240684a365718dce5a0b019
Dustin Dew <phyxashun@gmail.com>
2026-06-19 14:08:51 -0400

    Update

--- src/push.ts
+++ src/push.ts
@@ -1,4 +1,4 @@
-import { intro, outro, spinner } from '@clack/prompts';
+import { intro, outro, log } from '@clack/prompts';
 import pc from 'picocolors';
 import { spawnSync } from 'child_process';
 
@@ -17,31 +17,29 @@ async function main() {
 
     intro(`${pc.bgMagenta(pc.black(' 󰊢 Git Automation Script '))}`);
 
-    const s = spinner();
-
     // Execute 'git add .'
-    s.start('Staging changes (git add .)...');
+    log.step('Staging changes (git add .)...');
     if (!runCommand('git', ['add', '.'])) {
-        s.stop('❌ Failed to stage changes.\n', 1);
+        log.stop('❌ Failed to stage changes.\n', 1);
         process.exit(1);
     }
 
     // Execute 'git commit -m "MESSAGE"'
-    s.message(`Committing changes (git commit -m "${msg}")...`);
+    log.step(`Committing changes (git commit -m "${msg}")...`);
     if (!runCommand('git', ['commit', '-m', 'Update'])) {
         // Check if there was nothing to commit
-        s.stop('❌ Failed to commit. (Are there any new changes?)\n', 1);
+        log.stop('❌ Failed to commit. (Are there any new changes?)\n', 1);
         process.exit(1);
     }
 
     // Execute 'git push'
-    s.message('Pushing to origin main (git push origin main)...');
+    log.step('Pushing to origin main (git push origin main)...');
     if (!runCommand('git', ['push', 'origin', 'main'])) {
-        s.stop('❌ Failed to push changes to remote.\n', 1);
+        log.stop('❌ Failed to push changes to remote.\n', 1);
         process.exit(1);
     }
 
-    s.stop('All steps completed successfully!');
+    log.success('All steps completed successfully!');
     outro('✅ Git update complete!');
 }
 
