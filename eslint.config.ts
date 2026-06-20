68334aaf030e14e223658e55b9cfaf997366d071
Dustin Dew <phyxashun@gmail.com>
2026-06-20 00:19:32 -0400

    Update

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,4 @@
-// ~ FILE-PATH: /eslint.config.ts
+// FILE-PATH: eslint.config.ts
 
 import js from '@eslint/js';
 import tsParser from '@typescript-eslint/parser';
@@ -10,11 +10,22 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig([
     // Global ignores
-    globalIgnores(['.vscode/**', 'ALL/**', '**/node_modules/**', 'public/**', 'build/**', 'dist/**']),
+    globalIgnores([
+        '.vscode/**',
+        'ALL/**',
+        '**/node_modules/**',
+        'public/**',
+        'build/**',
+        'dist/**',
+    ]),
     {
         files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
 
-        extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
+        extends: [
+            js.configs.recommended,
+            ...tseslint.configs.recommended,
+            prettierConfig,
+        ],
 
         languageOptions: {
             parser: tsParser,
@@ -33,7 +44,11 @@ export default defineConfig([
 
         rules: {
             // Formatting
-            quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+            quotes: [
+                'error',
+                'single',
+                { avoidEscape: true, allowTemplateLiterals: true },
+            ],
             semi: ['error', 'always'],
 
             // Eslint
@@ -42,9 +57,15 @@ export default defineConfig([
             'no-control-regex': 'off',
 
             // TypeScript
-            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_' },
+            ],
             '@typescript-eslint/explicit-function-return-type': 'off',
-            '@typescript-eslint/no-explicit-any': ['warn', { ignoreRestArgs: false }],
+            '@typescript-eslint/no-explicit-any': [
+                'warn',
+                { ignoreRestArgs: false },
+            ],
         },
     },
     // PRETTIER MUST BE LAST
